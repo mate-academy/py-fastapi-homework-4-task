5f5ae0bcc3db9697feb9823e7406fba33ff9046a
Rabb1t-Artem <79219648+Rabb1t-Artem@users.noreply.github.com>
2025-01-23 13:07:21 +0200

    solution

---
+++ src/schemas/profiles.py
@@ -0,0 +1,52 @@
+import datetime
+from typing import Any, Optional
+
+from fastapi import File, Form, UploadFile
+from pydantic import BaseModel
+
+
+class ProfileRequestForm(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    gender: Optional[str]
+    date_of_birth: Optional[datetime.date]
+    info: Optional[str]
+    avatar: Optional[UploadFile]
+
+    @classmethod
+    def as_form(
+        cls,
+        first_name: Optional[str] = Form(),
+        last_name: Optional[str] = Form(),
+        gender: Optional[str] = Form(),
+        date_of_birth: Optional[datetime.date] = Form(),
+        info: Optional[str] = Form(),
+        avatar: Optional[UploadFile] = File(),
+    ) -> Any:
+        return cls(
+            first_name=first_name,
+            last_name=last_name,
+            gender=gender,
+            date_of_birth=date_of_birth,
+            info=info,
+            avatar=avatar,
+        )
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class ProfileResponseSchema(BaseModel):
+    id: int
+    user_id: int
+    first_name: Optional[str]
+    last_name: Optional[str]
+    gender: Optional[str]
+    date_of_birth: Optional[datetime.date]
+    info: Optional[str]
+    avatar: Optional[str]
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
